bb661df0e023f85e60c3fef91d539114f3cd9980
王司徒 <neilwang543@gmail.com>
2025-01-27 12:03:04 +0800

    feat: 去除console

--- components/Particles.tsx
+++ components/Particles.tsx
@@ -9,7 +9,6 @@ const MyParticles = ({ theme }: { theme?: string }) => {
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      console.log(engine, 'engine engineengineengineengine')
       // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
       // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
       // starting from v2 you can add only the features you need reducing the bundle size
